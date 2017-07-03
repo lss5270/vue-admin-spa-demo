19b987c0b8db3ac9b59a7999802549a59c184322
lss <446526232@qq.com>
2017-07-03 16:52:16 +0800

    7.3

---
+++ js/13.9cc2e2e8a9ca937eb1cb.js
@@ -0,0 +1,1 @@
+webpackJsonp([13],{249:function(e,n,o){var i=o(6)(o(267),null,null,null,null);e.exports=i.exports},267:function(e,n,o){"use strict";Object.defineProperty(n,"__esModule",{value:!0}),n.default={name:"authredirect",created:function(){var e=window.location.search.slice(1);window.opener.location.href=window.location.origin+"/login#"+e,window.close()}}}});
