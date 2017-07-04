989e301f4be4739a4893a2eea7f91161b549a146
lss <446526232@qq.com>
2017-07-04 16:26:47 +0800

    movie

---
+++ js/14.c915399f0d50ab761cfc.js
@@ -0,0 +1,1 @@
+webpackJsonp([14],{249:function(e,n,o){var i=o(6)(o(268),null,null,null,null);e.exports=i.exports},268:function(e,n,o){"use strict";Object.defineProperty(n,"__esModule",{value:!0}),n.default={name:"authredirect",created:function(){var e=window.location.search.slice(1);window.opener.location.href=window.location.origin+"/login#"+e,window.close()}}}});
