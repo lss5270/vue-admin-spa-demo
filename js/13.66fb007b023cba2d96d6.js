cf7b217520722f64d203e49de3901dbc03bfa203
lss <446526232@qq.com>
2017-07-08 15:26:41 +0800

    del

--- js/13.66fb007b023cba2d96d6.js
+++
@@ -1,1 +0,0 @@
-webpackJsonp([13],{246:function(e,n,o){var i=o(6)(o(264),null,null,null,null);e.exports=i.exports},264:function(e,n,o){"use strict";Object.defineProperty(n,"__esModule",{value:!0}),n.default={name:"authredirect",created:function(){var e=window.location.search.slice(1);window.opener.location.href=window.location.origin+"/login#"+e,window.close()}}}});
